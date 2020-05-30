00bd6aac7e08757ed600a9a717c6116079b1946f
Hiwonder <44456375+Hiwonder@users.noreply.github.com>
2020-05-30 15:43:48 +0800

    update

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ namespace ad_sensor {
     /**
     * Get the ad sensor value 
     */
-    //% weight=98 blockId=adSensorValue block="Get sensor (suport:sound,knob,light,rain drop,soil humidity,etc) pin %adPin ad value(0~255)"
+    //% weight=98 blockId=adSensorValue block="Get sensor (suport:sound,knob,light,rain drop,soil humidity,etc) pin|%adPin|ad value(0~255)"
     export function adSensorValue(adPin: AnalogPin): number {
         let adValue = pins.analogReadPin(adPin);
         adValue = adValue * 255 / 1023;
